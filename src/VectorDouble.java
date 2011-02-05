d5992cdf876dcdb48e20f214081cee68c7e0e011
Nicholas Dexter <nxd3734@glados.(none)>
2011-02-05 18:38:15 -0500

    Added gaussian elimination row reduction algorithm and backsubstitution solver

--- src/VectorDouble.java
+++ src/VectorDouble.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 public class VectorDouble {
 
@@ -7,6 +8,45 @@ public class VectorDouble {
     public VectorDouble( int size ) {
         this.size = size;
         this.data = new double[size];
+    }
+
+    /**
+     * Data copy constructor
+     *
+     * @param cdata The data to copy
+     */
+    public VectorDouble( double[] cdata ) {
+        this.size = cdata.length;
+
+        data = new double[size];
+        this.copyData( cdata );
+    }
+
+    /** 
+     * Copy the data from a given array into 
+     * the vector own allocated data.
+     *
+     * @param cdata The array to copy from
+     */
+    private void copyData( double[] cdata ) {
+        for ( int r = 0; r < size ; r++ ) {
+                this.data[r] = cdata[r];
+        }
+    }
+
+    /**
+     * Generates a new vector of the specified size, filled with random data.
+     *
+     * @param row The number of rows in the vector
+     * @return The new random Double Vector
+     */
+    public static VectorDouble random( int row ) {
+        VectorDouble rand = new VectorDouble( row ); 
+        Random prng = new Random( 1000 );
+        for ( int r = 0; r < rand.length(); r++ ) {
+            rand.data[r] = prng.nextDouble();
+        }
+        return rand;
     }
 
     public int length() {
